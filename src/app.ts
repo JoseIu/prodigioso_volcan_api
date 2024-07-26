d43006acf32f3f50d04720b8354cc68c941b133b
Joselu <joseluissgz97@gmail.com>
2024-07-26 23:32:48 +0200

    chore: Add CORS middleware to allow requests from specified origin

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
+import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
 import { conectDB } from './config/conectDB';
 import { loginRouter } from './routes/login.routes';
+
 conectDB();
 export const expressApp = express();
 
+const dominPermit = process.env.FRONT_URL;
+
+const corsOptions = {
+  origin: function (origin: string | undefined, callback: (err: Error | null, allow: boolean) => void) {
+    if (!origin || dominPermit!.indexOf(origin) !== -1) return callback(null, true);
+    else callback(new Error('No permitido por CORS'), false);
+  }
+};
+expressApp.use(cors(corsOptions));
 expressApp.use(express.json());
 expressApp.use(express.text());
 
