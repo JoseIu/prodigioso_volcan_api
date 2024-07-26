cc31528bfd66624cf90b223302d8648bfbb2cf91
Joselu <joseluissgz97@gmail.com>
2024-07-26 18:24:02 +0200

    add: userModel

---
+++ src/model/user.model.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import { UserInterface } from '../interfaces/user.interface';
+
+const userModel = new mongoose.Schema<UserInterface>({
+  iss: { type: String, required: true },
+  sub: { type: String, required: true },
+  email: { type: String, required: true },
+  email_verified: { type: Boolean, required: true },
+  name: { type: String, required: true },
+  given_name: { type: String, required: true },
+  picture: { type: String, required: true }
+});
+
+export const User = mongoose.model<UserInterface>('User', userModel);
