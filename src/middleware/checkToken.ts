60a6514ddc076fd9aff2c287b8c1cdab784d840b
Joselu <joseluissgz97@gmail.com>
2024-07-26 23:33:25 +0200

    fix: versionKey on model

--- src/middleware/checkToken.ts
+++ src/middleware/checkToken.ts
@@ -1,10 +1,10 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express';
 
 export const checkToken = (req: Request, res: Response, next: NextFunction) => {
-	const { authorization } = req.headers;
+  const { authorization } = req.headers;
 
-	if (!authorization || !authorization.startsWith("Bearer"))
-		return res.status(401).json({ message: "Token no valid" });
+  if (!authorization || !authorization.startsWith('Bearer'))
+    return res.status(401).json({ error: true, user: null, message: 'Token no valid' });
 
-	next();
+  next();
 };
--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,14 +1,17 @@
 import mongoose from 'mongoose';
 import { UserInterface } from '../interfaces/user.interface';
 
-const userModel = new mongoose.Schema<UserInterface>({
-  iss: { type: String, required: true },
-  sub: { type: String, required: true },
-  email: { type: String, required: true },
-  email_verified: { type: Boolean, required: true },
-  name: { type: String, required: true },
-  given_name: { type: String, required: true },
-  picture: { type: String, required: true }
-});
+const userModel = new mongoose.Schema<UserInterface>(
+  {
+    iss: { type: String, required: true },
+    sub: { type: String, required: true },
+    email: { type: String, required: true },
+    email_verified: { type: Boolean, required: true },
+    name: { type: String, required: true },
+    given_name: { type: String, required: true },
+    picture: { type: String, required: true }
+  },
+  { versionKey: false }
+);
 
 export const User = mongoose.model<UserInterface>('User', userModel);
