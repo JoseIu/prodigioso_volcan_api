0b1a5de51d6be761ea5041cfc4ea06c0a5405fab
Joselu <joseluissgz97@gmail.com>
2024-07-28 21:15:23 +0200

    add: login-googlev2 route

--- src/routes/login.routes.ts
+++ src/routes/login.routes.ts
@@ -1,7 +1,9 @@
 import express from 'express';
-import { loginGoogleController } from '../controller/login.controller';
+import { loginGoogleController, loginGoogleV2Controller } from '../controller/login.controller';
 import { checkToken } from '../middleware/checkToken';
 
 export const loginRouter = express.Router();
 
 loginRouter.post('/login-google', checkToken, loginGoogleController);
+
+loginRouter.post('/login-googlev2', loginGoogleV2Controller);
